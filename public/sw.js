b60ceb29663fa95362c5c996aceadc553e230811
vominht <vominhtu1007@gmail.com>
2026-01-17 18:36:50 +0700

    update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,11 +1,16 @@
-const CACHE_NAME = 'chitieu-v1';
+const CACHE_NAME = 'chitieu-v2';
 const ASSETS = [
   './',
   './index.html',
+  './manifest.json',
   './css/style.css',
+  './css/heatmap.css',
+  './css/dashboard.css',
   './js/app.js',
   './js/utils.js',
   './js/firebase.js',
+  './assets/icon-192.png',
+  './assets/icon-512.png',
   'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
   'https://fonts.googleapis.com/css2?family=Outfit:wght@300;400;500;600;700&display=swap'
 ];
